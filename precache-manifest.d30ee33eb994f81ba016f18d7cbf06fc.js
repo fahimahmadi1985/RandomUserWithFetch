a7b804df600d96409edea8db35220e0aab5dd01e
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-04 15:48:22 +0100

    Updates

---
+++ precache-manifest.d30ee33eb994f81ba016f18d7cbf06fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7f0c08c1a42716391b53e3ab496502",
+    "url": "/RandomUserWithFetch/index.html"
+  },
+  {
+    "revision": "f1a3918e92e0e25585a2",
+    "url": "/RandomUserWithFetch/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3c1a58a722df88fd6c59",
+    "url": "/RandomUserWithFetch/static/js/2.fb6fa6f2.chunk.js"
+  },
+  {
+    "revision": "f1a3918e92e0e25585a2",
+    "url": "/RandomUserWithFetch/static/js/main.ec552148.chunk.js"
+  },
+  {
+    "revision": "b6033895438523fafccb",
+    "url": "/RandomUserWithFetch/static/js/runtime-main.ae04a0d5.js"
+  }
+]);
